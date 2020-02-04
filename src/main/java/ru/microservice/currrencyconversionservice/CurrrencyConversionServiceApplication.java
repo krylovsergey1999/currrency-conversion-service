3d785a9c1c569c147f06507b3f6d70572ea3fa8d
Krylov Sergey <kssergeyru@gmail.com>
2020-02-04 15:19:52 +0300

    Initial

---
+++ src/main/java/ru/microservice/currrencyconversionservice/CurrrencyConversionServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.microservice.currrencyconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrrencyConversionServiceApplication.class, args);
+	}
+
+}
