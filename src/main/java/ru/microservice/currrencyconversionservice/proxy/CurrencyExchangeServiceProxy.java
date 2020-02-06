7196bc06bb34f5f2d0b8dd7095407d3e4e08ca7c
Krylov Sergey <kssergeyru@gmail.com>
2020-02-06 20:19:27 +0300

    Глобальный рефакторинг

--- src/main/java/ru/microservice/currrencyconversionservice/proxy/CurrencyExchangeServiceProxy.java
+++ src/main/java/ru/microservice/currrencyconversionservice/proxy/CurrencyExchangeServiceProxy.java
@@ -2,14 +2,13 @@ package ru.microservice.currrencyconversionservice.proxy;
 
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import ru.microservice.currrencyconversionservice.domain.CurrencyConversionBean;
 
-@FeignClient(name = "currency-exchange-service")
+@FeignClient(name = "netflix-zuul-api-gateway-server")
 @RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
-    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    @GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
     public CurrencyConversionBean retrieveEchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
 }
