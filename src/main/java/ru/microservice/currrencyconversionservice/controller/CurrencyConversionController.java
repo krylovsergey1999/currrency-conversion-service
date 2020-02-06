8ace58c4534ed24c9536ae4ce09bac395090df76
Krylov Sergey <kssergeyru@gmail.com>
2020-02-06 11:07:23 +0300

    Add RestTemplate

--- src/main/java/ru/microservice/currrencyconversionservice/controller/CurrencyConversionController.java
+++ src/main/java/ru/microservice/currrencyconversionservice/controller/CurrencyConversionController.java
@@ -4,24 +4,52 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import ru.microservice.currrencyconversionservice.domain.CurrencyConversionBean;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class CurrencyConversionController {
 
     @GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
     public ResponseEntity<CurrencyConversionBean> convertCurrency(@PathVariable BigDecimal quantity, @PathVariable String to, @PathVariable String from) {
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
+        ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversionBean.class, uriVariables);
+        CurrencyConversionBean responseEntityBody = responseEntity.getBody();
         CurrencyConversionBean conversion = CurrencyConversionBean.builder()
-                .id(1L)
+                .id(responseEntityBody.getId())
                 .from(from)
                 .to(to)
+                .conversionMultiple(responseEntityBody.getConversionMultiple())
                 .quantity(quantity)
-                .conversionMultiple(BigDecimal.ONE)
-                .totalCalculateAmount(quantity)
-                .port(0)
+                .totalCalculateAmount(quantity.multiply(responseEntityBody.getConversionMultiple()))
+                .port(responseEntityBody.getPort())
                 .build();
         return ResponseEntity.ok(conversion);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
